f3e1c8e3408914b06170b79068c5c46a37c422ed
Bishnu18 <bishnu.chintu18@gmail.com>
2024-04-04 20:08:16 +0530

    admin commit 3

--- src/Routers/AdminRouters.jsx
+++ src/Routers/AdminRouters.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Admin from '../Admin/components/Admin'
 
 const AdminRouters = () => {
   return (
     <div>
-        
+        <Routes>
+            <Route path='/' element={<Admin/>}></Route>
+        </Routes>
     </div>
   )
 }
