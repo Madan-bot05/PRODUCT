0f926538361edc27fbd22a749aae1c79e35ba46b
Bishnu18 <bishnu.chintu18@gmail.com>
2024-04-10 02:19:58 +0530

    some edit in dashboard and product table

--- src/Admin/components/Dashboard.jsx
+++ src/Admin/components/Dashboard.jsx
@@ -2,6 +2,7 @@ import { Grid } from '@mui/material'
 import React from 'react'
 import Achivement from './Achivement'
 import MonthlyOverview from './MonthlyOverview'
+import ProductsTable from './ProductsTable'
 
 const AdminDashboard = () => {
   return (
@@ -15,8 +16,10 @@ const AdminDashboard = () => {
            </Grid>
            <Grid item xs={12} md={8}> 
  <MonthlyOverview/>          
-
-           </Grid>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            
+          </Grid>
 
 
 
